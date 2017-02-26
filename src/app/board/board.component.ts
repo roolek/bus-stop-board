7b85971fbb76e7aa5c30035f98521a388fdbae7e
Rolek <rolek93@gmail.com>
2017-02-26 22:41:00 +0100

    Dodanie odpytywania.

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,21 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PekaApiService } from '../peka/peka-api.service';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import 'rxjs/add/observable/interval';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/startWith';
+
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.css']
 })
-export class BoardComponent implements OnInit {
+export class BoardComponent implements OnInit, OnDestroy {
   public name: string;
   public times: any[];
-
+  readonly POLLING_INTERVAL: number = 5000;
+  private timesSubscription: Subscription;
   constructor(private pekaService: PekaApiService) { }
 
+
+
   onBollardChange(symbol: string) {
-    this.pekaService.getTimes(symbol).subscribe((res) => {
+    if (this.timesSubscription)
+      this.timesSubscription.unsubscribe();
+    this.timesSubscription = this.startPolling(symbol).subscribe((res) => {
       this.times = res;
     })
+  }
+
+  startPolling(symbol: string) {
+    return Observable.interval(this.POLLING_INTERVAL).startWith(0)
+      .switchMap(() => this.getTimes(symbol))
+  }
+
+  getTimes(symbol: string) {
+    return this.pekaService.getTimes(symbol);
   }
 
   onSelect(name) {
@@ -23,6 +43,11 @@ export class BoardComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    if (this.timesSubscription)
+      this.timesSubscription.unsubscribe();
   }
 
 }
--- src/app/times-board/times-board.component.ts
+++ src/app/times-board/times-board.component.ts
@@ -1,9 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit,  
+  state,
+  style,
+  transition,
+  animate } from '@angular/core';
 
 @Component({
   selector: 'app-times-board',
   templateUrl: './times-board.component.html',
-  styleUrls: ['./times-board.component.css']
+  styleUrls: ['./times-board.component.css'],
 })
 export class TimesBoardComponent implements OnInit {
   @Input() times: any;
